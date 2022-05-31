5b9745608da4ffec10dc74599e5940b860546032
xNesMilo <camilonintendo@gmail.com>
2022-05-30 22:17:35 -0400

    tarea01_mod02_asincronica

---
+++ ejercicio01_mod02_asincronico/script.js
@@ -0,0 +1,22 @@
+//Edad + dias vividos
+const edad = prompt('Ingresa tu edad ')
+
+const edadxdias = (edad * 365)
+
+const mensaje = ('He vivido ' + edadxdias + ' dias')
+
+alert(mensaje)
+
+//Segunda consulta
+
+const snack = prompt('Cual es tu snack favorito')
+
+alert('Tu snack favorito es: ' + (snack))
+
+const edadmaxima = 83
+
+const valor = parseInt(prompt('Ingrese el valor de su snack favorito'))
+
+alert('Necesitaras $' + ' ' + (edad-83*valor) + ' para que te alcancen hasta los ' + (edadmaxima) + ' años de edad' )
+
+
